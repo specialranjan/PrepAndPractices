cac80cb5e93bf605192e683d412f94e025462866
Ranjan Tiwari <specialranjan@gmail.com>
2020-03-19 20:18:06 +0530

    Adding DotNETCore Project

---
+++ Dot NET Core/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cakes";
+
+            //return View(_pieRepository.AllPies);
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
